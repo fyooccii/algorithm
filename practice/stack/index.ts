f167619104f89912be845ca8c1be37db83df17d6
fy <fyooccii@gmail.com>
2022-03-16 14:31:46 +0800

    fix:优化stack操作逻辑

--- practice/stack/index.ts
+++ practice/stack/index.ts
@@ -1,38 +1,58 @@
 class Stack<T> {
-    constructor(data?: T[]) {
-        this._data = data || [];
-        this._size = data ? data.length : 0;
+    constructor(size: number) {
+        this._data = new Array(size);
+        this._size = size;
+        this._length = 0;
+        this._top = null;
     }
     private _data: T[];
     private _size: number;
-    get size() {
-        return this._size;
-    }
+    private _length: number;
+    private _top: number;
 
-    push(val: T) {
-        this._data.push(val);
-        this._size++;
+    push(val: T): boolean {
+        // stack满了
+        if (this._length === this._size) return false;
+        // 首次像stack中添加数据
+        if (this._top === null) {
+            this._data[0] = val;
+            this._top = 0;
+        } else {
+            this._data[++this._top] = val;
+        }
+        this._length++;
+        return true;
     }
 
     pop(): T {
-        this._size--;
-        return this._data.pop();
+        // 如果此次出栈后，栈中无数据
+        if (this._top - 1 < 0) {
+            this._top = null;
+            this._length = 0;
+            return this._data[0];
+        }
+        this._length--;
+        return this._data[this._top--];
     }
 
     printStack() {
-        console.log(this._data);
+        if (this._top !== null) {
+            console.log(this._data.slice(0, this._top + 1));
+        } else {
+            console.log('stack is empty');
+        }
     }
 }
 
-const s = new Stack<string>(['1', '2']);
-s.printStack();
-console.log('===');
-console.log('push');
-s.push('3');
-s.printStack();
-console.log('===');
-console.log('pop');
-const result = s.pop();
-console.log(result);
-s.printStack();
-console.log(s.size);
+const s = new Stack<string>(3);
+s.push('1');
+s.push('2');
+console.log(s);
+console.log('pop', s.pop());
+console.log('pop', s.pop());
+console.log(s);
+s.push('1');
+s.push('2');
+s.push('1');
+s.push('2');
+console.log(s);
